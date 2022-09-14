f9c25a7227c7d1db393299d299a7a8416c346288
SoNomad <nurmuhammad16203@gmail.com>
2022-09-14 10:25:16 +0300

    Return fix 2

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -58,7 +58,7 @@ module.exports.usersController = {
       const user = await User.findById(req.params.user_id);
       const book = await Book.findById(req.params.book_id);
 
-      if (!user.rentedBooks.includes()) {
+      if (!user.rentedBooks.includes(req.params.book_id)) {
         return res.json(`Книга ${book.name} не найдена в списке арендованных`);
       }
 
